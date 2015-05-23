283968051f3759ad3b5c762289f4f6988e8ffe78
Bryce Caine <brycecaine@gmail.com>
2015-05-23 02:55:48 -0600

    Modified prep_entries.sh to play nicely with CapsaNotes

--- prep_entries.sh
+++ prep_entries.sh
@@ -5,17 +5,57 @@ rm -fr content
 mkdir content
 cp -r raw/* content
 
-echo 'start'
+echo 'Start'
 
-for f in $(find /home/brycecaine/journalblog/content -name '*.md')
+for file in $(find /home/brycecaine/journalblog/content -name '*.md')
 do
-	# Remove comments at top of file
-	sed -i '/<!---/d' $f
-	sed -i '/-->/d' $f
+    echo "Begin: $file"
+    # -------------------------------------------------------------------------
+    # Generate front matter
+
+    # Get parent directory name to assign as author
+    path=$(dirname $file)
+    path_els=(${path//// })
+    name_el="${path_els[4]}"
+    name=$(echo "$name_el" | tr _ " " | sed 's/[^ ]\+/\L\u&/g')
+
+    # Get datetime of file
+    date_str=$(sed -n 's/\$notedate \(.*\) -->/\1/p' $file)
+    date=$(date -d $date_str +"%Y-%m-%d %H:%M")
+    file_date=$(date -d $date_str +"%Y.%m.%d.%H.%M")
+
+    # Get title
+    title="Journal of $name $date"
+
+    # Get tags
+    tags_str=$(sed -n 's/\(:.*:\s\)/\1/p' $file)
+    tags_els=(${tags_str//:/ })
+
+    tags=""
+    for i in "${tags_els[@]}"
+    do
+        :
+        if [ -z "$tags" ]; then
+            tags="$i"
+        else
+            tags="$tags, $i"
+        fi
+    done
+
+    # Insert front matter into file
+    sed -i "1iTitle: $title\nDate: $date\nAuthor: $name\nTags: $tags\n" $file
+
+    # -------------------------------------------------------------------------
+    # Moddify syntax for images
 
 	# Add {filename} prefix to image links
-	sed -i 's/{filename}//g' $f
-	sed -i 's/!\[image\](/!\[image\]({filename}/g' $f
+	sed -i 's/{filename}//g' $file
+	sed -i 's/!\[image\](/!\[image\]({filename}/g' $file
+
+    # -------------------------------------------------------------------------
+    # Change filename
+    mv "$file" "content/$name_el/journal_${name_el}_$file_date.md"
+    echo "End: $file"
 done
 
-echo 'done'
+echo 'Done'
